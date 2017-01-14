0d3c4110301dda05e6fccfe48739272e04daecad
Ryan Allen <Ryan Allen>
2017-01-14 00:52:06 -0500

    Added persistent session storage

--- todos/imports/startup/client/routes.js
+++ todos/imports/startup/client/routes.js
@@ -1,6 +1,9 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
+import { Session } from 'meteor/session';
+import { Users } from '../../api/users/users.js';
+
 // Import to load these templates
 import '../../ui/layouts/app-body.js';
 import '../../ui/pages/root-redirector.js';
@@ -12,6 +15,7 @@ import '../../ui/accounts/accounts-templates.js';
 
 FlowRouter.route('/lists/:_id', {
   name: 'Lists.show',
+  triggersEnter: [acquireSession],
   action() {
     BlazeLayout.render('App_body', { main: 'Lists_show_page' });
   },
@@ -19,6 +23,39 @@ FlowRouter.route('/lists/:_id', {
 
 FlowRouter.route('/', {
   name: 'App.home',
+  triggersEnter: [acquireSession],
+  action() {
+    BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
+  },
+});
+
+FlowRouter.route('/jukebox/', {
+  name: 'Jukebox.home',
+  triggersEnter: [acquireSession],
+  action() {
+    BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
+  },
+});
+
+FlowRouter.route('/jukebox/create/', {
+  name: 'Jukebox.create',
+  triggersEnter: [acquireSession],
+  action() {
+    BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
+  },
+});
+
+FlowRouter.route('/jukebox/join/', {
+  name: 'Jukebox.join',
+  triggersEnter: [acquireSession],
+  action() {
+    BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
+  },
+});
+
+FlowRouter.route('/jukebox/playlist/:_id', {
+  name: 'Jukebox.playlist',
+  triggersEnter: [acquireSession],
   action() {
     BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
   },
@@ -29,4 +66,13 @@ FlowRouter.notFound = {
   action() {
     BlazeLayout.render('App_body', { main: 'App_notFound' });
   },
+};
+
+function acquireSession() {
+  const sessionKey = "jukebox-active-user-id";
+  var userIdFromSession = Session.get(sessionKey);
+  if(!userIdFromSession){
+    // var userId = Users.createNewUser();
+    // Session.setPersistent(sessionKey, userId);
+  }
 };
