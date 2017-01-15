ead0adbdaad7e8168f5d58132720446dcff3c870
Ryan Allen <Ryan Allen>
2017-01-15 16:09:43 -0500

    Create page functioning with playlist selection

--- imports/create-page.js
+++ imports/create-page.js
@@ -40,21 +40,33 @@ Template.create_page.helpers({
 });
 
 Template.create_page.events({
-  'click #js-new-playlist'() {
-  	var playlistName = $('#playlist-name').val();
+  'click li'(event) {
+    var playlistName = $('#playlist-name').val();
+    // we set the spotifyId on the actual list element to it is easy to access here without referencing the meteor collection
+    var spotifyPlaylistId = event.target.id;
 
-    var songs = [];
-    // TODO: read this via CSS selector from the page to get the actual selected playlistId (right now we are just grabbing a populated one arbitrarily)
-    var biggestPlayist = SpotifyPlaylists.findOne({}, {sort: {songCount: -1}});
-    if(biggestPlayist){
-      var selectedPlaylistId = biggestPlayist.spotifyId;
-      getSongsForPlaylist(selectedPlaylistId, function(response){
+    var playlistSongs = [];
+    function savePlaylistAndSongs(name, songs){
+      var playlistId = HostedPlaylists.insert({
+        name: name,
+        userId: Session.get("jukebox-active-user-id")
+      })
+      var newPlaylist = HostedPlaylists.findOne(playlistId);
+      newPlaylist.initializeSongs(songs);
+
+      // then redirect to the new playlist
+      FlowRouter.go('Jukebox.playlist', { _id: newPlaylist.publicId });
+    };
+    
+    // if a spotify playlist was chosen, then populate songs from that
+    if(spotifyPlaylistId){
+      getSongsForPlaylist(spotifyPlaylistId, function(response){
         var rawSongs = response.items;
 
-        // convert response to songs to insert
+        // convert response to playlistSongs to insert
         $.each(rawSongs, function( index, value ) {
           var track = value.track;
-          songs.push({
+          playlistSongs.push({
             name: track.name,
             artist: track.artists[0].name,
             // track.id is the ID portion only - while track.uri has the 'spotify:track:' prefix as well
@@ -62,21 +74,13 @@ Template.create_page.events({
           });
         });
 
-        // var songs = [{
-        //   spotifyId: "spotify:track:2WMRd3xAb9FwXopCRNWDq1",
-        //     name: "Song A",
-        //     artist: "Artist A"
-        // }];
-
-        var playlistId = HostedPlaylists.insert({
-          name: playlistName,
-          userId: Session.get("jukebox-active-user-id")
-        })
-        var newPlaylist = HostedPlaylists.findOne(playlistId);
-        newPlaylist.initializeSongs(songs);
-        
-        FlowRouter.go('Jukebox.playlist', { _id: newPlaylist.publicId });
+        // save and redirect
+        savePlaylistAndSongs(playlistName, playlistSongs);
       });
     }
-  },
+    // otherwise, submit with an empty playlist
+    else{
+      savePlaylistAndSongs(playlistName, playlistSongs);
+    }
+  }
 });
